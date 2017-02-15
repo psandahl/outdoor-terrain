649dc0b57e674048810e2c1a307c423ef7c0adf4
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-02-15 22:26:55 +0100

    Simple example with red square.

--- src/Main.hs
+++ src/Main.hs
@@ -5,11 +5,17 @@ import           Data.Maybe       (fromJust, isNothing)
 import           Graphics.UI.GLFW (OpenGLProfile (..), StickyKeysInputMode (..),
                                    Window, WindowHint (..))
 import qualified Graphics.UI.GLFW as GLFW
+import           Linear
 import           System.Exit      (exitFailure)
 
 import           RenderLoop       (renderLoop)
 
-import           Graphics.LWGL    (ClearBufferMask (..))
+import           Graphics.LWGL    (BufferTarget (..), BufferUsage (..),
+                                   ClearBufferMask (..), ComponentCount (..),
+                                   GLfloat, Location (..), PrimitiveType (..),
+                                   Program, ShaderType (..),
+                                   VertexArrayObject (..),
+                                   VertexAttribPointerType (..))
 import qualified Graphics.LWGL    as GL
 
 createGLContext :: Int -> Int -> IO Window
@@ -33,14 +39,59 @@ createGLContext width height = do
 
     return $ fromJust window
 
+loadShaders :: [(ShaderType, FilePath)] -> IO Program
+loadShaders xs = do
+    eShaders <- GL.loadShaders xs
+    case eShaders of
+        Right program -> return program
+        Left err -> do
+            putStrLn err
+            GLFW.terminate
+            exitFailure
+
+vertices :: [V3 GLfloat]
+vertices =
+    [ V3   0.5    0.5  0
+    , V3 (-0.5)   0.5  0
+    , V3 (-0.5) (-0.5) 0
+    , V3   0.5    0.5  0
+    , V3 (-0.5) (-0.5) 0
+    , V3   0.5  (-0.5) 0
+    ]
+
+initGraphics :: IO VertexArrayObject
+initGraphics = do
+    [vao] <- GL.glGenVertexArray 1
+    GL.glBindVertexArray vao
+
+    [vbo] <- GL.glGenBuffers 1
+    GL.glBindBuffer ArrayBuffer vbo
+    GL.glBufferDataList ArrayBuffer vertices StaticDraw
+
+    GL.glEnableVertexAttribArray (Location 0)
+    GL.glVertexAttribPointer (Location 0) Three GLFloat False 0 0
+
+    return vao
+
 main :: IO ()
 main = do
-  window <- createGLContext 1024 768
-  GLFW.makeContextCurrent (Just window)
-  GLFW.setStickyKeysInputMode window StickyKeysInputMode'Enabled
+    window <- createGLContext 1024 768
+    GLFW.makeContextCurrent (Just window)
+    GLFW.setStickyKeysInputMode window StickyKeysInputMode'Enabled
 
-  GL.glClearColor 0 0 0.4 0
-  renderLoop window $ \_ ->
-    GL.glClear [ColorBuffer]
+    program <- loadShaders [ (VertexShader, "shaders/lighted-box.vert")
+                           , (FragmentShader, "shaders/lighted-box.frag")]
 
-  GLFW.terminate
+    vao <- initGraphics
+
+    GL.glClearColor 0 0 0.4 0
+    renderLoop window $ \_ -> do
+        GL.glClear [ColorBuffer]
+
+        GL.glUseProgram program
+        GL.glBindVertexArray vao
+        GL.glDrawArrays Triangles 0 6
+
+        GL.glBindVertexArray (VertexArrayObject 0)
+
+    GLFW.terminate
