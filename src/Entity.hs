2568c0acea2d6ec0db717f9eefaaeba31e374e19
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-02-19 00:17:20 +0100

    Some basic Entity structure.

---
+++ src/Entity.hs
@@ -0,0 +1,81 @@
+module Entity
+    ( Entity (..)
+    --, EntityFactory
+    --, initFactory
+    , makeFloor
+    , render
+    ) where
+
+import           Control.Monad              (forM_, when)
+import           Control.Monad.IO.Class     (liftIO)
+import           Control.Monad.Trans.Either
+import           Data.Either
+import           Linear                     (V2 (..), V3 (..))
+
+import           Graphics.LWGL
+import           Graphics.LWGL.Vertex_P_Tex
+
+data Entity = Entity
+    { program  :: !Program
+    , vao      :: !VertexArrayObject
+    , animate  :: (Entity -> IO Entity)
+    , textures :: ![(Texture, TextureTarget, Location)]
+    , elements :: !Int
+    }
+
+--data EntityFactory = EntityFactory
+
+--initFactory :: IO (Either String EntityFactory)
+--initFactory = return $ Right EntityFactory
+
+makeFloor :: IO (Either String Entity)
+makeFloor = runEitherT $ do
+    eProgram <- liftIO $
+        loadShaders [(VertexShader, "shaders/lighted-box.vert")
+                    , (FragmentShader, "shaders/lighted-box.frag")]
+    when (isLeft eProgram) $ do
+        let Left err = eProgram
+        left err
+    let Right prog = eProgram
+
+    eTexture <- liftIO $ loadTexture2D "textures/stones.jpg" RGB8 False
+    when (isLeft eTexture) $ do
+        let Left err = eTexture
+        left err
+    let Right texture = eTexture
+
+    vao' <- liftIO $ makeVertexArrayObject StaticDraw floorVertices
+    texLoc <- liftIO $ glGetUniformLocation prog "tex1"
+
+    right Entity
+        { program = prog
+        , vao = vao'
+        , animate = \e -> return e
+        , textures = [(texture, Texture2D, texLoc)]
+        , elements = length floorVertices
+        }
+
+
+render :: Entity -> IO ()
+render entity = do
+    glUseProgram $ program entity
+    glBindVertexArray $ vao entity
+
+    forM_ (zip (textures entity) [0 ..]) $ \((tex, targ, loc), unit) -> do
+        glActiveTexture $ TextureUnit (fromIntegral unit)
+        glBindTexture targ tex
+        glUniform1i loc unit
+
+    glDrawArrays Triangles 0 $ elements entity
+
+    glBindVertexArray (VertexArrayObject 0)
+
+floorVertices :: [Vertex]
+floorVertices =
+    [ Vertex { position = V3   0.5    0.5  0, texCoord = V2 1 1 }
+    , Vertex { position = V3 (-0.5)   0.5  0, texCoord = V2 0 1 }
+    , Vertex { position = V3 (-0.5) (-0.5) 0, texCoord = V2 0 0 }
+    , Vertex { position = V3   0.5    0.5  0, texCoord = V2 1 1 }
+    , Vertex { position = V3 (-0.5) (-0.5) 0, texCoord = V2 0 0 }
+    , Vertex { position = V3   0.5  (-0.5) 0, texCoord = V2 1 0 }
+    ]
--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,17 @@
 module Main where
 
-import           Control.Monad              (when)
-import           Data.Maybe                 (fromJust, isNothing)
-import           Graphics.UI.GLFW           (OpenGLProfile (..),
-                                             StickyKeysInputMode (..), Window,
-                                             WindowHint (..))
-import qualified Graphics.UI.GLFW           as GLFW
-import           Linear                     (V2 (..), V3 (..))
-import           System.Exit                (exitFailure)
+import           Control.Monad    (when)
+import           Data.Either      (isLeft)
+import           Data.Maybe       (fromJust, isNothing)
+import           Graphics.UI.GLFW (OpenGLProfile (..), StickyKeysInputMode (..),
+                                   Window, WindowHint (..))
+import qualified Graphics.UI.GLFW as GLFW
+import           Linear           (V2 (..), V3 (..))
+import           System.Exit      (exitFailure)
 
-import           RenderLoop                 (renderLoop)
-
-import           Graphics.LWGL              (BufferUsage (..),
-                                             ClearBufferMask (..),
-                                             PrimitiveType (..), Program,
-                                             ShaderType (..), Texture,
-                                             TextureFormat (..),
-                                             TextureTarget (..),
-                                             TextureUnit (..),
-                                             VertexArrayObject (..))
-import qualified Graphics.LWGL              as GL
-import           Graphics.LWGL.Vertex_P_Tex (Vertex (..), makeVertexArrayObject)
+import           Entity
+import           Graphics.LWGL    (ClearBufferMask (..), glClear, glClearColor)
+import           RenderLoop       (renderLoop)
 
 createGLContext :: Int -> Int -> IO Window
 createGLContext width height = do
@@ -43,68 +34,27 @@ createGLContext width height = do
 
     return $ fromJust window
 
-loadShaders :: [(ShaderType, FilePath)] -> IO Program
-loadShaders xs = do
-    eShaders <- GL.loadShaders xs
-    case eShaders of
-        Right program -> return program
-        Left err -> do
-            putStrLn err
-            GLFW.terminate
-            exitFailure
-
-loadTexture :: FilePath -> IO Texture
-loadTexture file = do
-    eTexture <- GL.loadTexture2D file RGB8 False
-    case eTexture of
-        Right texture -> return texture
-        Left err -> do
-            putStrLn err
-            GLFW.terminate
-            exitFailure
-
-vertices :: [Vertex]
-vertices =
-    [ Vertex { position = V3   0.5    0.5  0, texCoord = V2 1 1 }
-    , Vertex { position = V3 (-0.5)   0.5  0, texCoord = V2 0 1 }
-    , Vertex { position = V3 (-0.5) (-0.5) 0, texCoord = V2 0 0 }
-    , Vertex { position = V3   0.5    0.5  0, texCoord = V2 1 1 }
-    , Vertex { position = V3 (-0.5) (-0.5) 0, texCoord = V2 0 0 }
-    , Vertex { position = V3   0.5  (-0.5) 0, texCoord = V2 1 0 }
-    ]
-
-initGraphics :: IO VertexArrayObject
-initGraphics = do
-    vao <- makeVertexArrayObject StaticDraw vertices
-    return vao
-
 main :: IO ()
 main = do
     window <- createGLContext 1024 768
     GLFW.makeContextCurrent (Just window)
     GLFW.setStickyKeysInputMode window StickyKeysInputMode'Enabled
 
-    program <- loadShaders [ (VertexShader, "shaders/lighted-box.vert")
-                           , (FragmentShader, "shaders/lighted-box.frag")]
+    eEntity <- makeFloor
+    when (isLeft eEntity) $ do
+        let Left err = eEntity
+        putStrLn err
+        GLFW.terminate
+        exitFailure
 
-    vao <- initGraphics
-    texture <- loadTexture "textures/stones.jpg"
+    let Right entity = eEntity
 
-    GL.glClearColor 0 0 0.4 0
-    renderLoop window $ \_ -> do
-        GL.glClear [ColorBuffer]
-
-        GL.glUseProgram program
-
-        GL.glActiveTexture $ TextureUnit 0
-        GL.glBindTexture Texture2D texture
-
-        texLoc <- GL.glGetUniformLocation program "tex1"
-        GL.glUniform1i texLoc 0
-
-        GL.glBindVertexArray vao
-        GL.glDrawArrays Triangles 0 6
-
-        GL.glBindVertexArray (VertexArrayObject 0)
+    glClearColor 0 0 0.4 0
+    renderLoop window $ renderFrame entity
 
     GLFW.terminate
+
+renderFrame :: Entity -> Window -> IO ()
+renderFrame entity _ = do
+    glClear [ColorBuffer]
+    render entity
