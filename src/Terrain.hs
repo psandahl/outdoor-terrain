852345e0ee614c39e5aa867fcab376a0c5fe4cd4
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-02-23 21:41:02 +0100

    Added index drawing.

--- src/Terrain.hs
+++ src/Terrain.hs
@@ -8,7 +8,16 @@ module Terrain
     ) where
 
 import           Control.Monad              (forM_)
-import           Graphics.LWGL
+import           Graphics.LWGL              (BufferUsage (..),
+                                             CullFaceMode (..),
+                                             EnableCapability (..), GLuint,
+                                             Location, Mesh (..), Program,
+                                             ShaderType (..), Texture,
+                                             TextureFormat (..),
+                                             TextureTarget (..),
+                                             TextureUnit (..),
+                                             VertexArrayObject (..))
+import qualified Graphics.LWGL              as GL
 import           Graphics.LWGL.Vertex_P_Tex (Vertex (..))
 import           Linear                     (V2 (..), V3 (..))
 
@@ -25,18 +34,18 @@ data Patch = Patch
 
 initTerrain :: IO (Either String Terrain)
 initTerrain = do
-    eProgram <- loadShaders [ (VertexShader, "shaders/terrain.vert")
-                            , (FragmentShader, "shaders/terrain.frag")
-                            ]
+    eProgram <- GL.loadShaders [ (VertexShader, "shaders/terrain.vert")
+                               , (FragmentShader, "shaders/terrain.frag")
+                               ]
     case eProgram of
         Right prog -> do
             eTextures <-
-                sequence <$> mapM (loadTexture2D RGB8 False)
+                sequence <$> mapM (GL.loadTexture2D RGB8 False)
                     ["textures/grass.jpg"]
             case eTextures of
                 Right ts -> do
                     locations <-
-                        mapM (glGetUniformLocation prog)
+                        mapM (GL.glGetUniformLocation prog)
                             ["grassTexture"]
 
                     return $ Right Terrain
@@ -50,7 +59,7 @@ initTerrain = do
         Left err -> return $ Left err
 
 newPatch :: IO Patch
-newPatch = Patch <$> buildFromList StaticDraw vertices
+newPatch = Patch <$> GL.buildFromList StaticDraw vertices indices'
 
 addPatch :: Terrain -> Patch -> Terrain
 addPatch terrain patch =
@@ -58,29 +67,31 @@ addPatch terrain patch =
 
 render :: Terrain -> IO ()
 render terrain = do
-    glUseProgram $ program terrain
-    glEnable CullFace
-    glCullFace Back
+    GL.glUseProgram $ program terrain
+    GL.glEnable CullFace
+    GL.glCullFace Back
 
     forM_ (zip3 [0 ..]
                 (textures terrain)
                 (textureLocations terrain)) $ \(unit, texture, loc) -> do
-        glActiveTexture $ TextureUnit (fromIntegral unit)
-        glBindTexture Texture2D texture
-        glUniform1i loc unit
+        GL.glActiveTexture $ TextureUnit (fromIntegral unit)
+        GL.glBindTexture Texture2D texture
+        GL.glUniform1i loc unit
 
     forM_ (patches terrain) $ \patch -> do
-        glBindVertexArray (vao $ mesh patch)
-        glDrawArrays Triangles 0 (numVertices $ mesh patch)
+        GL.glBindVertexArray (vao $ mesh patch)
+        GL.drawTrianglesVector (indices $ mesh patch)
 
-    glBindVertexArray (VertexArrayObject 0)
+    GL.glBindVertexArray (VertexArrayObject 0)
 
 vertices :: [Vertex]
 vertices =
     [ Vertex { position = V3   0.5    0.5  0, texCoord = V2 1 1 }
     , Vertex { position = V3 (-0.5)   0.5  0, texCoord = V2 0 1 }
     , Vertex { position = V3 (-0.5) (-0.5) 0, texCoord = V2 0 0 }
-    , Vertex { position = V3   0.5    0.5  0, texCoord = V2 1 1 }
-    , Vertex { position = V3 (-0.5) (-0.5) 0, texCoord = V2 0 0 }
     , Vertex { position = V3   0.5  (-0.5) 0, texCoord = V2 1 0 }
     ]
+
+indices' :: [GLuint]
+indices' =
+    [0, 1, 2, 0, 2, 3]
