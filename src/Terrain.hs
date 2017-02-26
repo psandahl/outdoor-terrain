75808ddf7c54d2c52918d26ac521d934cb789989
Patrik Sandahl <kosmoskatten.software@gmail.com>
2017-02-26 12:00:38 +0100

    Took away texture stuff.

--- src/Terrain.hs
+++ src/Terrain.hs
@@ -7,26 +7,22 @@ module Terrain
     , render
     ) where
 
-import           Control.Monad              (forM_)
-import           Graphics.LWGL              (BufferUsage (..),
-                                             EnableCapability (..), GLfloat,
-                                             GLuint, Location, Mesh (..),
-                                             PolygonFace (..), Program,
-                                             ShaderType (..), Texture,
-                                             TextureFormat (..),
-                                             TextureTarget (..),
-                                             TextureUnit (..),
-                                             VertexArrayObject (..))
-import qualified Graphics.LWGL              as GL
-import           Graphics.LWGL.Vertex_P_Tex (Vertex (..))
-import           Linear                     (M44, V2 (..), V3 (..), (!*!))
+import           Control.Monad          (forM_)
+import           Graphics.LWGL          (BufferUsage (..),
+                                         EnableCapability (..), GLfloat, GLuint,
+                                         Location, Mesh (..), PolygonFace (..),
+                                         Program, ShaderType (..), Texture,
+                                         TextureFormat (..), TextureTarget (..),
+                                         TextureUnit (..),
+                                         VertexArrayObject (..))
+import qualified Graphics.LWGL          as GL
+import           Graphics.LWGL.Vertex_P (Vertex (..))
+import           Linear                 (M44, V2 (..), V3 (..), (!*!))
 
 data Terrain = Terrain
-    { program          :: !Program
-    , textures         :: ![Texture]
-    , textureLocations :: ![Location]
-    , mvpLocation      :: !Location
-    , patches          :: ![Patch]
+    { program     :: !Program
+    , mvpLocation :: !Location
+    , patches     :: ![Patch]
     } deriving Show
 
 data Patch = Patch
@@ -40,25 +36,12 @@ initTerrain = do
                                ]
     case eProgram of
         Right prog -> do
-            eTextures <-
-                sequence <$> mapM (GL.loadTexture2D RGB8 False)
-                    ["textures/grass.jpg"]
-            case eTextures of
-                Right ts -> do
-                    textureLocations' <-
-                        mapM (GL.glGetUniformLocation prog)
-                            ["grassTexture"]
-
-                    mvpLocation' <- GL.glGetUniformLocation prog "mvp"
-
-                    return $ Right Terrain
+            mvpLocation' <- GL.glGetUniformLocation prog "mvp"
+            return $ Right Terrain
                         { program = prog
-                        , textures = ts
-                        , textureLocations = textureLocations'
                         , mvpLocation = mvpLocation'
                         , patches = []
                         }
-                Left err -> return $ Left err
 
         Left err -> return $ Left err
 
@@ -78,13 +61,6 @@ render perspective view terrain = do
     let mvp = perspective !*! view
     GL.setMatrix4 (mvpLocation terrain) mvp
 
-    forM_ (zip3 [0 ..]
-                (textures terrain)
-                (textureLocations terrain)) $ \(unit, texture, loc) -> do
-        GL.glActiveTexture $ TextureUnit (fromIntegral unit)
-        GL.glBindTexture Texture2D texture
-        GL.glUniform1i loc unit
-
     forM_ (patches terrain) $ \patch -> do
         GL.glBindVertexArray (vao $ mesh patch)
         GL.drawTrianglesVector (indices $ mesh patch)
@@ -93,10 +69,10 @@ render perspective view terrain = do
 
 vertices :: [Vertex]
 vertices =
-    [ Vertex { position = V3   0.5    0.5  0, texCoord = V2 1 1 }
-    , Vertex { position = V3 (-0.5)   0.5  0, texCoord = V2 0 1 }
-    , Vertex { position = V3 (-0.5) (-0.5) 0, texCoord = V2 0 0 }
-    , Vertex { position = V3   0.5  (-0.5) 0, texCoord = V2 1 0 }
+    [ Vertex { position = V3   0.5    0.5  0 }
+    , Vertex { position = V3 (-0.5)   0.5  0 }
+    , Vertex { position = V3 (-0.5) (-0.5) 0 }
+    , Vertex { position = V3   0.5  (-0.5) 0 }
     ]
 
 indices' :: [GLuint]
